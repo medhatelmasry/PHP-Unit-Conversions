599349fdfd277eba528f655b89d1cd6d6cf7e0e3
Medhat Elmasry <medhat@elmasry.ca>
2013-11-14 17:35:37 -0800

    First commit of the PHP Unit Conversions application

---
+++ temperature/celsius2fahrenheit_process.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Celsius to Fahrenheit Conversion Result</title>
+    </head>
+    <body>
+    <dir>
+    <?php
+        if (isset($_POST["celcius"]) == TRUE) {
+            $c = $_POST["celcius"];
+            $f = ($c *  9/5) + 32;
+            echo "$c degrees Celcius is $f degrees Fahrenheit.";
+        }
+        else {
+            echo "You must enter a valid value for the temperature in Celcius.";
+        }
+    ?>
+    </dir>
+    </body>
+</html>
